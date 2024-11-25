54b5fa3a6f8093574231e82ed86e0d540ad5fe9a
SuvegesMarton <suvegesmarton@gmail.com>
2024-11-25 22:54:49 +0100

    gravity fixed

--- scripts/roundManager.gd
+++ scripts/roundManager.gd
@@ -3,7 +3,7 @@ extends Node2D
 var RocketScene = preload("res://scenes/rocket.tscn")
 var MeteoriteScene = preload("res://scenes/meteorite.tscn")
 
-var G = 6.67 * (10 ** -11)
+var G = 6.67 * (10.0 ** -3)
 
 var physicsElements = []
 var rocket
@@ -20,14 +20,14 @@ func _ready() -> void:
 	var m1 = MeteoriteScene.instantiate() as Node2D
 	m1.position = Vector2(800, 300)
 	m1.velocity = Vector2(0, 0)
-	m1.weight = 1
+	m1.weight = 100000000
 	add_child(m1)
 	physicsElements.append(m1)
 	
 	var m2 = MeteoriteScene.instantiate() as Node2D
 	m2.position = Vector2(500, 300)
 	m2.velocity = Vector2(0, 0)
-	m2.weight = 0
+	m2.weight = 100000000
 	add_child(m2)
 	physicsElements.append(m2)
 	#for i in range
@@ -36,21 +36,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	calculateGravityEffects(delta)
 	
-	
-	
-	
-	
-	
-	
+
 func calculateGravityEffects(delta:float) -> void:
 	for outer in physicsElements:
 		for inner in physicsElements:
-			outer.forces += calculateGravityForce(outer, inner) * delta
+			if outer != inner:
+				outer.forces += calculateGravityForce(outer, inner) * delta
 			
 #calculate the forces acting upon "outer"
 func calculateGravityForce(outer: Node2D, inner: Node2D) -> Vector2:
 	#vector between the 2 objects
-	var direction = Vector2(outer.position.x-inner.position.x, outer.position.y-inner.position.y)
+	var direction = Vector2(inner.position.x-outer.position.x, inner.position.y-outer.position.y)
 	var distanceSquared = direction.x**2+direction.y**2
 	#gravity equation
 	var forceMagnitude = (G*outer.weight*inner.weight)/distanceSquared
